d67e2d7da7f0be9d3306ea65372a4e388a3a640e
ilamam <ilamam@harpaitalia.it>
2023-02-19 14:41:22 +0100

    unit test company service

--- tests/Unit/CompanyControllerTest.php
+++ tests/Unit/CompanyControllerTest.php
@@ -17,7 +17,9 @@ class CompanyControllerTest extends TestCase
 
     public function test_createCompanyService()
     {
+        $this->assertEquals(0,User::all()->count());
         $user = User::factory()->create();
+        $this->assertEquals(1,User::all()->count());
         $this->actingAs($user);
 
         $data = [
@@ -30,8 +32,47 @@ class CompanyControllerTest extends TestCase
             'active' => 1,
             'type' => 1,
         ];
-
+        
+        $this->assertEquals(0,Company::all()->count());
+        
         $company = (new CompanyService)->createCompanyService($data['user_id'], $data['businessName'], $data['address'], $data['vat'], $data['taxCode'], $data['employees'], $data['active'], $data['type']);
+        
+        $this->assertEquals(1,Company::all()->count());
+
+        $this->assertInstanceOf(Company::class, $company);
+        $this->assertEquals($data['businessName'], $company->businessName);
+        $this->assertEquals($data['address'], $company->address);
+        $this->assertEquals($data['vat'], $company->vat);
+        $this->assertEquals($data['taxCode'], $company->taxCode);
+        $this->assertEquals($data['employees'], $company->employees);
+        $this->assertEquals($data['active'], $company->active);
+        $this->assertEquals($data['type'], $company->type);
+    }
+
+    public function test_createCompanyService_user_not_auth()
+    {
+        $this->assertEquals(0,User::all()->count());
+        $user = User::factory()->create();
+        $this->assertEquals(1,User::all()->count());
+        $this->actingAs($user);
+        $this->assertAuthenticated();
+
+        $data = [
+            'user_id' => Auth::id(),
+            'businessName' => $this->faker->company,
+            'address' => $this->faker->company,
+            'vat' =>  '01234567891',
+            'taxCode' => $this->faker->regexify('[A-Z]{6}[0-4]{5}'),
+            'employees' => $this->faker->randomNumber(3, false),
+            'active' => 1,
+            'type' => 1,
+        ];
+        
+        $this->assertEquals(0,Company::all()->count());
+        
+        $company = (new CompanyService)->createCompanyService($data['user_id'], $data['businessName'], $data['address'], $data['vat'], $data['taxCode'], $data['employees'], $data['active'], $data['type']);
+        
+        $this->assertEquals(1,Company::all()->count());
 
         $this->assertInstanceOf(Company::class, $company);
         $this->assertEquals($data['businessName'], $company->businessName);
@@ -44,17 +85,30 @@ class CompanyControllerTest extends TestCase
     }
 
     public function test_showCompanyService(){
+        $this->assertEquals(0,User::all()->count());
         $user = User::factory()->create();
+        $this->assertEquals(1,User::all()->count());
         $this->actingAs($user);
+        $this->assertAuthenticated();
+
+        $this->assertEquals(0,Company::all()->count());
         $company = Company::factory()->create()->toArray();
+        $this->assertEquals(1,Company::all()->count());
         $response = (new CompanyService)->showCompanyService($company['id']);
         $this->assertNotEmpty($response);
     }
 
     public function test_updateCompanyService(){
+        $this->assertEquals(0,User::all()->count());
         $user = User::factory()->create();
+        $this->assertEquals(1,User::all()->count());
         $this->actingAs($user);
+        $this->assertAuthenticated();
+
+        $this->assertEquals(0,Company::all()->count());
         $company = Company::factory()->create();
+        $this->assertEquals(1,Company::all()->count());
+
         $new_address = 'via topolino';
         $response = (new CompanyService)->updateCompanyService(['address' => $new_address], $company);
         $this->assertNotEmpty($response);
@@ -62,9 +116,15 @@ class CompanyControllerTest extends TestCase
     }
 
     public function test_destroyCompanyService(){
+        $this->assertEquals(0,User::all()->count());
         $user = User::factory()->create();
+        $this->assertEquals(1,User::all()->count());
         $this->actingAs($user);
+        $this->assertAuthenticated();
+
+        $this->assertEquals(0,Company::all()->count());
         $company = Company::factory()->create();
+        $this->assertEquals(1,Company::all()->count());
         (new CompanyService)->destroyCompanyService($company);
         $this->assertEquals(0,Company::all()->count());
     }
