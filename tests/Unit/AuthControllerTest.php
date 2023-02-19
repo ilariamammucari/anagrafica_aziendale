6023af1c0c05852d4227c9ba030ba2c1310cda05
ilamam <ilamam@harpaitalia.it>
2023-02-19 12:49:31 +0100

    unit test auth service

--- tests/Unit/AuthControllerTest.php
+++ tests/Unit/AuthControllerTest.php
@@ -12,14 +12,25 @@ class AuthControllerTest extends TestCase
     use RefreshDatabase;
 
     public function test_registerAuthService(){
+        $this->assertEquals(0,User::all()->count());
         $new_user = User::factory()->make();
-        $user = (new AuthService)->registerAuthService($new_user['name'], $new_user['email'], $new_user['password']);
-        $this->assertNotEmpty($user);
+        $response = (new AuthService)->registerAuthService($new_user['name'], $new_user['email'], $new_user['password']);
+        $this->assertEquals(1,User::all()->count());
+        $this->assertNotEmpty($response);
+        $this->assertEquals($response->name, $new_user->name);
+        $this->assertEquals($response->email, $new_user->email);
     }
 
     public function test_loginAuthService(){
         $user = User::factory()->create()->toArray();
         $response = (new AuthService)->loginAuthService($user['email'], 'password');
         $this->assertNotEmpty($response);
+        $this->assertNull($response->original);
+    }
+
+    public function test_loginAuthService_wrong_credentials(){
+        $user = User::factory()->create()->toArray();
+        $response = (new AuthService)->loginAuthService($user['email'], 'passwordErrata');
+        $this->assertEquals($response->original['message'],'Credenziali errate');
     }
 }
